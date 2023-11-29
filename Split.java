3da5061e705d7d3b0e846e01dc9c78f627ef1934
R Kumar Shaanu <kumarshaanu619007@gmail.com>
2023-11-29 11:34:12 +0530

    Update Split.java

--- Split.java
+++ Split.java
@@ -28,3 +28,49 @@ class Split
         }
     }  
 }
+/**import java.util.*;
+class Work
+{   
+    String S;
+    Scanner in=new Scanner(System.in);
+    String input()
+    {
+        S=in.nextLine();
+        return S;
+    }
+}
+class Split 
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n;
+        System.out.println("Enter number of people");
+        n=in.nextInt();
+        String arr[]=new String[n];
+        Work w=new Work();
+        System.out.println("Enter name");
+        String str;
+        int i;
+        for(i=0;i<n;i=i+1)
+        {
+            arr[i]=w.input();
+            //str=arr[i];
+            //System.out.println(str);
+        }int a;int b;int pay[n][]
+        System.out.println("Enter choice 1.Input 2.Total");
+        a=in.nextInt();
+        switch(a)
+        {
+            case 1:
+                System.out.println("Choose name");
+                for(i=0;i<n;i=i+1)
+                {
+                    str=arr[i];
+                    System.out.println(+(i+1)+""+str);
+                }b=in.nextInt();
+                
+                break;
+        }
+    }  
+}*/
